37a74b96c7a5b75531e615a75290275eb6249f28
Pedro Rodríguez <p_alex96@hotmail.com>
2021-02-03 01:00:21 -0600

    modificación extra en el código

---
+++ testforgit.py
@@ -0,0 +1,11 @@
+print('ya más o menos lo recuerdo jeje')
+
+#agregaré un comentario y una impresión de variables
+a=23
+print(f'esta es mi edad: {a}!!!')
+
+#b = true
+#if b:
+#    print('has adquirido un año más')
+
+print("algo extra...")
